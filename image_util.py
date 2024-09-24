a62d804d3368273a04b9718a883014fbbedafe19
Andrew T. Smith <andrew.tombras.smith@gmail.com>
2024-09-23 19:24:58 -0700

    refactoring test image class, need to tune corner detection params so both images have similar numbers.

--- image_util.py
+++ image_util.py
@@ -1,174 +1,238 @@
+"""
+Class for representing synthetic images for testing homography estimation.
+Uses Harris corner detection to find corners in the image, and extracts local feature descriptors (histograms)
+for each corner.  The descriptors can be compared using the Symetrized Kulback-Leibler divergence.
+"""
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from make_test_imgs import draw_img, transform_img
 
 
-def make_image_pair(size, noise_sigma=0.1, n_rects=10, n_circle_colors=3, n_rect_colors=3):
+class TestImage(object):
     """
-    Generate an image that will be easy to analyze with a toy feature detector & matcher:
-        * Draw 1000 circles of random sizes and colors
-        * Draw N rectangles of random sizes and colors on top of the circles
-        * Limit the number of colors to make it harder to match features
+    Class for synthetic image homography testing.
 
-    Transform the image with a random rotation, translation, and scaling, and add noise.
-
-    :param size: the size of the image (width, height)
-    :param noise_sigma: the standard deviation of the noise to add to the image
-    :param n_rects: the number of rectangles to draw on top of the circles
+    Generate a test image with a known transformation between it and a second image.
+    Perform (Harris) corner detection tuned for this kind of image.
+    Extract local feature descriptors (represented internally as histograms, somewhat invariant to this restricted set of images)
+    Compare two descriptors (using Symetrized Kulback-Leibler divergence) to return a score.
     """
-    img = np.zeros((size[1], size[0], 3), dtype=np.float32)
-    min_dim = np.min(size)
 
-    # make image with circles and rectangles
-    circle_rad_range = [min_dim//10, min_dim//5]
-    circle_palette = np.random.rand(n_circle_colors, 3)
-    circle_colors = circle_palette[np.random.randint(0, n_circle_colors, 1000)]
-    for i in range(1000):
-        center = np.random.rand(2) * size
-        radius = int(np.random.uniform(*circle_rad_range))
-        color = circle_colors[i]
-        cv2.circle(img, tuple(center.astype(int)), radius,
-                   color, -1, lineType=cv2.LINE_AA)
+    def __init__(self, size, n_rects=10, n_circle_colors=30, n_rect_colors=3):
+        """
+        :param size: the size of the image (width, height)
+        :param n_rects: the number of rectangles to draw on top of the circles
+        :param n_circle_colors: the number of colors to use for the circles
+        :param n_rect_colors: the number of colors to use for the rectangles
+        """
+        self.size=size
+        self.img, self.palette = draw_img(size, n_rects, n_circle_colors, n_rect_colors)
+        self._init()
 
-    rect_palette = np.random.rand(n_rect_colors, 3)
-    rect_colors = rect_palette[np.random.randint(0, n_rect_colors, n_rects)]
-    rect_size_range = [min_dim//15, min_dim//9]
-    for i in range(n_rects):
-        center = np.random.rand(2) * size
-        width, height = np.random.uniform(*rect_size_range, 2)
-        color = rect_colors[i]
-        cv2.rectangle(img, tuple(center.astype(int)), tuple(
-            (center + [width, height]).astype(int)), color, -1, lineType=cv2.LINE_AA)
+    def _init(self):
+        self.n_colors = self.palette.shape[0]
+        self.rgb_img = self.palette[self.img].astype(self.palette.dtype)
+        self.gray = cv2.cvtColor(self.rgb_img, cv2.COLOR_BGR2GRAY).astype(np.float32)
 
-    # Make an affine transformation
-    transform_angle = np.pi/6
-    transform_scale = 1.5 + np.random.rand() * 0.5
-    transform_translation = np.random.randn(2) * 40
-    M = cv2.getRotationMatrix2D(
-        (size[0]//2, size[1]//2), transform_angle*180/np.pi, transform_scale)
-    M[:, 2] += transform_translation
-    img2 = cv2.warpAffine(img, M, size)
-    img2 += np.random.randn(*img.shape) * noise_sigma
+    def transform(self, noise_frac):
+        """
+        Create anew TestImage from self.
+        :param image: a 2d array of pixel values, each in [0, palette.shape[0])
+        :param noise_frac: the fraction of pixels to randomly change
+        """
+        img2, transf  = transform_img(self.img, noise_frac, max_color_ind=self.n_colors)
+        r = TestImage(size=self.size)
+        r.img = img2
+        r.palette = self.palette
+        r._init()
+        return r, transf
 
-    # convert to uint8
-    img = (255*np.clip(img, 0, 1)).astype(np.uint8)
-    img2 = (255*np.clip(img2, 0, 1)).astype(np.uint8)
+    @staticmethod
+    def compare_descriptors(hist1, hist2):
+        return 0.5 * (np.sum(hist1 * np.log(hist1 / hist2)) + np.sum(hist2 * np.log(hist2 / hist1)))
 
-    return img, img2, {'M': M, 'angle': transform_angle, 'scale': transform_scale, 'translation': transform_translation}
-
-
-def find_corners(img, margin=0):
-    """
-    Find corners in an image using the Harris corner detector.
-    :param margin: don't return corners within this many pixels of the edge
-    """
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = np.float32(gray)
-
-    # Detect corners
-    dst = cv2.cornerHarris(gray, 3, 5, k=0.04) # increasing K decreases the number of corners
-
-    # Dilate to mark the corners
-    dst = cv2.dilate(dst, None)
-
-    # Threshold for an optimal value, it may vary depending on the image.
-    #dst[dst > 0.01*dst.max()] = [0, 0, 255]
-
-    # Get sub-pixel accuracy on the corners
-    ret, dst = cv2.threshold(dst, 0.01*dst.max(), 255, 0)
-    dst = np.uint8(dst)
-    ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
-
-    # Define the criteria to stop and refine the corners
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-    corners = cv2.cornerSubPix(gray, np.float32(centroids), (5, 5), (-1, -1), criteria)
-
-    # Filter out corners near the edge
-    corners = corners[(corners[:, 0] > margin) & (corners[:, 0] < img.shape[1]-margin) &
-                      (corners[:, 1] > margin) & (corners[:, 1] < img.shape[0]-margin)] 
-    return corners
-
-def _get_corner_windows(img, corners, window_size):
-    """
-    Get the window around each corner.
-    """
-    window_size = window_size // 2
-    corner_windows = []
-    for corner in corners:
-        x, y = corner
+    def get_patch(self, x, y, patch_size, which='index'):
+        if which not in ['index', 'rgb']:
+            raise ValueError("which must be one of 'index' or 'rgb'")
+        patch_size = patch_size // 2
         x, y = int(x), int(y)
-        window = img[y-window_size:y+window_size, x-window_size:x+window_size]
-        corner_windows.append(window)
+        src = self.img if which == 'index' else self.rgb_img
+        return src[y-patch_size:y+patch_size, x-patch_size:x+patch_size]
 
-    return corner_windows
+    def get_patch_descriptor(self, x, y, patch_size, smoothing=0.1):
+        """
+        Get the histogram of the patch around a point.
+        :param x: x-coordinate of the center of the patch
+        :param y: y-coordinate of the center of the patch
+        :param patch_size: the size of the patch (square, will cover x,y +/- patch_size//2)
+        :param smoothing: a small value to add to each bin to avoid dividing by zero in the histogram comparison
+        """
+        window = self.get_patch(x, y, patch_size)
+        hist = np.array([np.sum(window == i) for i in range(self.n_colors)]).reshape(-1)
+        hist += smoothing
+        hist /= np.sum(hist)
+        return hist
 
-def get_window_histogram(window, n_bins = 3, smoothing=0.1):
+    def find_corners(self, margin=0, harris_kwargs=None):
+        """
+        Find corners in an image using the Harris corner detector.
+        :param margin: don't return corners within this many pixels of the edge
+        :param harris_kwargs: additional keyword arguments to pass to cv2.cornerHarris
+            - blockSize: the size of the window to consider for each corner
+            - ksize: the size of the Sobel kernel to use for the derivative (must be odd)
+            - k: a free parameter in the Harris detector equation (increasing K decreases the number of corners)
+
+        :returns: an Nx2 array of corner coordinates
+        """
+        default_harris_kwargs = dict(blockSize=2,
+                                     ksize=3,
+                                     k=0.04)
+        if harris_kwargs is not None:
+            default_harris_kwargs.update(harris_kwargs)
+
+        dst = cv2.cornerHarris(self.gray, **default_harris_kwargs)
+
+        # Dilate to mark the corners
+        dst = cv2.dilate(dst, None)
+
+        # Threshold for an optimal value, it may vary depending on the image.
+        # dst[dst > 0.01*dst.max()] = [0, 0, 255]
+
+        # Get sub-pixel accuracy on the corners
+        ret, dst = cv2.threshold(dst, 0.01*dst.max(), 255, 0)
+        dst = np.uint8(dst)
+        ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+
+        # Define the criteria to stop and refine the corners
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+        corners = cv2.cornerSubPix(self.gray, np.float32(
+            centroids), (5, 5), (-1, -1), criteria)
+
+        # Filter out corners near the edge
+        corners = corners[(corners[:, 0] > margin) & (corners[:, 0] < self.img.shape[1]-margin) &
+                          (corners[:, 1] > margin) & (corners[:, 1] < self.img.shape[0]-margin)]
+        return corners
+
+
+def _test_corner_detector():
     """
-    Compute a histogram of the window using all three channels, the full range, and the specified number of bins.
-    :param window: a 3-channel image window
-    :param n_bins: the number of bins to use in each channel
-    :param smoothing: a small value to add to each bin to avoid dividing by zero in the histogram comparison
+    Create a 400x400 test image pair, find corners, and show the results in a plot.
+    return images and detected corners
     """
-    hist = cv2.calcHist([window], [0, 1, 2], None, [n_bins, n_bins, n_bins], [0, 256, 0, 256, 0, 256])
-    hist = hist.flatten() + smoothing
-    hist /= np.sum(hist)
-    return hist
+    args = dict(blockSize=2,
+                ksize=3,
+                k=0.04)
+
+    q_img1 = TestImage((400, 400))
+    q_img2, transf = q_img1.transform(0.02)
+    img1, img2 = q_img1.rgb_img, q_img2.rgb_img
+
+    # _disp_image_pair(q_img1, q_img2)
+
+    # find corners in both images & plot them
+    corners1 = q_img1.find_corners(margin=10, harris_kwargs=args) 
+    corners2 = q_img2.find_corners(margin=10, harris_kwargs=args)
+
+    fig, ax = plt.subplots(1, 2)
+    ax[0].imshow(cv2.cvtColor(img1, cv2.COLOR_BGR2RGB))
+    ax[0].scatter(corners1[:, 0], corners1[:, 1], color='r', s=50)
+    ax[0].set_title('Image 1')
+    ax[1].imshow(cv2.cvtColor(img2, cv2.COLOR_BGR2RGB))
+    ax[1].scatter(corners2[:, 0], corners2[:, 1], color='r', s=50)
+    ax[1].set_title('Image 2')
+
+    plt.show()
+
+    return (q_img1, corners1), (q_img2, corners2)
 
 
-def compare_histograms(hist1, hist2):
-    """
-    Use the "symatrized Kullback-Leibler divergence" to compare two histograms.
-    (i.e. as the distance between multinoimal distributions)
-    """
-    return 0.5 * np.sum(hist1 * np.log(hist1/hist2) + hist2 * np.log(hist2/hist1))
-
-def _test_image_dataset():
+def _test_similarity_metric(data1, data2):
     """
     Create a dataset, image pair. find corners, extract histograms, and create pairwise similarity matrix.
-    Plot the image and its corners.
+    Show a grid showing up to the first 10 of those corner windows on the top row, and below each, the
+           highest & lowest N matching corners from the other image in descending order.
+    :param data1: a tuple (QuantizedColorImage, corners) for the first image (and detected corners)
+    :param data2: a tuple (QuantizedColorImage, corners) for the second image (and detected corners
 
-    Plot the upper right 5x5 corner of the similarity matrix, above each row show the corresponding corner patch, 
-    and to the left of each column show the corresponding corner patch.
-      
     """
-    img, _, _ = make_image_pair((500, 500), noise_sigma=0.1)
-    corners = find_corners(img, margin=10)
-    n_bins = 3
-    window_size = 10
-    
-    windows = _get_corner_windows(img, corners, window_size)
-    
-    histograms = np.array([get_window_histogram(window, n_bins=n_bins) for window in windows])
-    
+    n_color_bins = 5
+    n_cols_max = 8
 
-    similarity_matrix = np.array([[compare_histograms(hist1, hist2) for hist2 in histograms] for hist1 in histograms])
+    # Number of examples to show (set to 1 to see the best and worst, etc)
+    n_ex = 2
+    qimg1, corners1 = data1
+    qimg2, corners2 = data2
+    img1, img2 = qimg1.rgb_img, qimg2.rgb_img
 
-    # Plot the image and its corners
-    plt.imshow(img)
-    plt.scatter(corners[:, 0], corners[:, 1], c='r')
-    plt.title("Image & Corners")
+    window_size = 20
+    # Extract histograms for each corner
+    windows1 = _get_corner_windows(img1, corners1, window_size)
+    windows2 = _get_corner_windows(img2, corners2, window_size)
 
+    hist1 = np.array([get_window_histogram(window, n_bins=n_color_bins)
+                     for window in windows1])
+    hist2 = np.array([get_window_histogram(window, n_bins=n_color_bins)
+                     for window in windows2])
 
-    # Plot the similarity matrix
-    fig, ax = plt.subplots()
-    ax.imshow(similarity_matrix, cmap='gray')
-    ax.set_title("Similarity Matrix")
-    ax.set_xlabel("Image 2 Corners")
-    ax.set_ylabel("Image 1 Corners")
+    # Compare histograms
+    similarity = np.array([[compare_histograms(h1, h2)
+                          for h2 in hist2] for h1 in hist1])
 
-    # Plot the upper right 5x5 corner of the similarity matrix
-    fig, ax = plt.subplots(5, 5)
-    for i in range(5):
-        for j in range(5):
-            ax[i, j].imshow(windows[i])
-            ax[i, j].set_title("Image 1 Corner %i" % i)
-            ax[i, j].axis('off')
-    for j in range(5):
-        ax[0, j].set_title("Image 2 Corner %i" % j, pad=20)
-        ax[0, j].imshow(windows[j])
-        ax[0, j].axis('off')
+    # show the first few corners of image1 and (up to) their top 8 matches in image2, in descending order of similarity
+    n_cols = np.min([n_cols_max, len(corners1)])
+    n_rows = 1 + 2 * n_ex
+    fig, ax = plt.subplots(n_rows, n_cols)
+    for i in range(n_cols):
+        ax[0, i].imshow(cv2.cvtColor(windows1[i], cv2.COLOR_BGR2RGB))
+        ax[0, i].axis('off')
+
+        def _plot_match(j, idx):
+            ax[j, i].imshow(cv2.cvtColor(windows2[idx], cv2.COLOR_BGR2RGB))
+            ax[j, i].axis('off')
+            ax[j, i].text(0, window_size, '%.2f' %
+                          similarity[i, idx], color='k', fontsize=10, va='top')
+
+        # show the best
+        for j, idx in enumerate(np.argsort(similarity[i])[:n_ex]):
+            _plot_match(j+1, idx)
+
+        # show the worst
+        for j, idx in enumerate(np.argsort(similarity[i])[-n_ex:]):
+            _plot_match(j+n_ex+1, idx)
+
+    # Annotate plots window with separation lines under first row and between best/worst rows.
+    # get the y-coordinates between the first and second row in figure coordinates.
+    _, y1 = ax[1, 0].transAxes.transform([0, 1.15])
+    _, y2 = ax[n_ex, 0].transAxes.transform([0, -.20])
+    y1 = fig.transFigure.inverted().transform([0, y1])[1]
+    y2 = fig.transFigure.inverted().transform([0, y2])[1]
+    # x limits are 5% and 95% of the figure width
+    x1 = 0.05
+    x2 = 0.90
+    # annotate
+    ax[1, 0].annotate('', xy=(x1, y1), xytext=(
+        x2, y1), xycoords='figure fraction', arrowprops=dict(arrowstyle='-', color='k'))
+    ax[n_ex, 0].annotate('', xy=(x1, y2), xytext=(
+        x2, y2), xycoords='figure fraction', arrowprops=dict(arrowstyle='-', color='k'))
+
+    # Add titles to the three row sections (off to the left)
+    ax[0, 0].text(-0.1, 0.5, 'image1 corners', fontsize=12, color='r',
+                  ha='right', va='center', transform=ax[0, 0].transAxes)
+    ax[1, 0].text(-0.1, 0.5, 'best %i matches\nof image2' % (n_ex, ), fontsize=12,
+                  color='r', ha='right', va='center', transform=ax[1, 0].transAxes)
+    ax[n_ex+1, 0].text(-0.1, 0.5, 'worst %i matches\nof image2' % (n_ex, ), fontsize=12,
+                       color='r', ha='right', va='center', transform=ax[n_ex+1, 0].transAxes)
+
+    # remove most space between subplots
+    plt.subplots_adjust(wspace=.1, hspace=.4)
+
+    # show and return
     plt.show()
 
 
 if __name__ == "__main__":
-    _test_image_dataset()
+    plt.ion()  # do all windows on startup
+    i1, i2 = _test_corner_detector()
+    #_test_similarity_metric(i1, i2)
+    plt.pause(0)
---
+++ make_test_imgs.py
@@ -0,0 +1,124 @@
+import numpy as np
+import cv2
+import pprint
+
+
+def draw_img(size, n_rects=10, n_circle_colors=30, n_rect_colors=3):
+    """
+    Generate an image that will be easy to analyze with a toy feature detector & matcher:
+        * Draw 1000 circles of random sizes and colors  (Background)
+        * Draw N rectangles of random sizes and colors on top of the circles (Foreground)
+
+    Every pixel will have a color that is either black, one of the circle colors, or one of the rectangle colors.
+    This high level of color qantization will make it easy to compare histograms of the color distribution in the images.
+    (i.e. to remove the curse of dimensionality in the full 256x256x256 color space)
+
+    :param size: the size of the image (width, height)
+    :param n_rects: the number of rectangles to draw on top of the circles
+    :param n_circle_colors: the number of colors to use for the circles
+    :param n_rect_colors: the number of colors to use for the rectangles
+
+    :returns: WxH - the image, indices into the color palette, and
+              Px3 - the color palette
+    """
+    # image pixel values will be indices into the color palette
+    circle_palette = np.random.rand(n_circle_colors, 3).astype(np.float32)
+    rect_palette = np.random.rand(n_rect_colors, 3).astype(np.float32)
+    full_palette = np.vstack([[[0, 0, 0]], circle_palette, rect_palette]).astype(np.float32)
+
+    img = np.zeros((size[1], size[0]), dtype=np.uint8)
+
+    # Random shape params must be in these ranges
+    min_dim = np.min(size)
+    rect_size_range = [min_dim//15, min_dim//6]
+    circle_rad_range = [min_dim//10, min_dim//5]
+
+    # make image with circles...
+    circle_colors = np.random.randint(0, n_circle_colors, 1000)
+    for i in range(1000):
+        center = np.random.rand(2) * size
+
+        radius = int(np.random.uniform(*circle_rad_range))
+        color = int(circle_colors[i])
+        cv2.circle(img, tuple(center.astype(int)), radius, color, -1)
+
+    # ...and rectangles
+    rect_colors = np.random.randint(0, n_rect_colors, n_rects)
+    for i in range(n_rects):
+        center = np.random.rand(2) * size
+        width, height = np.random.uniform(*rect_size_range, 2)
+        color = int(rect_colors[i]) + n_circle_colors
+        cv2.rectangle(img, tuple(center.astype(int)), tuple(
+            (center + [width, height]).astype(int)), color, -1)
+    return img, (full_palette * 255).astype(np.uint8)
+
+
+def _test_draw_img():
+    img, palette = draw_img((256, 256))
+    img_rgb = palette[img].astype(np.uint8)
+    cv2.imshow('image', img_rgb)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def transform_img(img, noise_frac=0., max_color_ind=None):
+    """
+    Transform the image by applying a random affine transformation to the image and palette.
+
+    :param img: WxH - the image
+    :param max_color_ind:  largest color value, i.e. palette size
+
+    :returns: WxH - the transformed image, and
+              {'M': transformation matrix, 
+               'angle': float, rotation angle, 
+               'scale': float, 
+               'translation': 2x1 )  - the transformation used
+    """
+
+    # Make an affine transformation & apply it to create the second image
+    size = (img.shape[1], img.shape[0])
+    transform_angle = np.pi/3 + np.random.rand() * np.pi/3
+    transform_scale = 1.1 + np.random.rand() * 0.5
+    transform_translation = np.random.randn(2) * 40
+    M = cv2.getRotationMatrix2D(
+        (size[0]//2, size[1]//2), transform_angle*180/np.pi, transform_scale)
+    M[:, 2] += transform_translation
+
+    # don't do any interpolation between color values, since they are indices
+    img2 = cv2.warpAffine(img, M, size, flags=cv2.INTER_NEAREST, borderValue=int(img[0, 0]))
+
+    # Colors are not aranged in any order so if the transform interpolates pallette indices, artifacts will show.
+    c2 = set(img2.reshape(-1))
+    c1 = set(img.reshape(-1))
+    assert c2.issubset(c1), "Transform added a color not in the original image:  %s" % (c2 - c1,)
+
+    # Add noise by changing some pixels to another color
+    x = img2.copy()
+    if noise_frac > 0:
+        max_color_ind = max_color_ind if max_color_ind is not None else np.max(img) + 1
+        noise = np.where(np.random.rand(*img.shape) < noise_frac)
+        noise_colors = np.random.randint(
+            0, max_color_ind, noise[0].size)
+        img2[noise] = noise_colors
+    return img2, {'M': M,
+                  'angle': transform_angle,
+                  'scale': transform_scale,
+                  'translation': transform_translation}
+
+
+def _test_transform_img():
+    img, palette = draw_img((256, 256))
+    img2, transform = transform_img(img, noise_frac=0.1, max_color_ind=palette.shape[0])
+    img_both = np.zeros((img.shape[0], img.shape[1]*2), dtype=np.uint8)
+    img_both[:, :img.shape[1]] = img
+    img_both[:, img.shape[1]:] = img2
+    img_both_rgb = palette[img_both].astype(np.uint8)
+
+    cv2.imshow('image', img_both_rgb)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    _test_draw_img()
+    _test_transform_img()
